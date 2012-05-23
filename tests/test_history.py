7427a289ba91fcc1e85e70a7079d785932014401
Ilan Schnell <ischnell@enthought.com>
2012-05-23 10:24:27 -0500

    ENH: update history tests

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,7 +1,7 @@
 import unittest
 from os.path import dirname, join
 
-from enstaller.history import History, find_revision
+from enstaller.history import History
 
 
 PATH = join(dirname(__file__), 'history')
@@ -12,25 +12,6 @@ class TestHistory(unittest.TestCase):
     def setUp(self):
         self.history = History('<dummy prefix>')
         self.history._log_path = PATH
-
-    def test_find_revision(self):
-        times = [
-            '2011-08-01 21:17:22 CDT',
-            '2011-08-01 22:38:37 CDT',
-            '2011-08-01 23:05:07 CDT',
-        ]
-        for dt, res in [
-            ('2011-08-01 21:17:21 CDT', 0),
-            ('2011-08-01 21:17:22 CDT', 0),
-            ('2011-08-01 21:17:23 CDT', 0),
-            ('2011-08-01 22:38:36 CDT', 0),
-            ('2011-08-01 22:38:37 CDT', 1),
-            ('2011-08-01 22:38:38 CDT', 1),
-            ('2011-08-01 23:05:06 CDT', 1),
-            ('2011-08-01 23:05:07 CDT', 2),
-            ('2011-08-01 23:05:08 CDT', 2),
-            ]:
-            self.assertEqual(find_revision(times, dt), res)
 
     def test_get_state(self):
         self.assertEqual(self.history.get_state(0),
