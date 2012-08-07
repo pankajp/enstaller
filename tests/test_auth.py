e7ab75ac3ac17119b10edd0e200dab389920b9d0
Robert David Grant <rgrant@enthought.com>
2012-08-07 16:58:36 -0500

    Improve auth unit tests using mock.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,128 +1,65 @@
 import unittest
+from mock import patch
+
 import enstaller.config as config
-from functools import partial
 
 
-users = {('jdoe', 'password1'): dict(first_name="Jane", last_name="Doe",
-            is_authenticated=True, has_subscription=True),  # EPD Basic and up
-         ('jsmith', 'password2'): dict(first_name="John", last_name="Smith",
-             is_authenticated=True, has_subscription=False),  # EPD Free
-         }
-
-badusers = (('fakeuser1', 'fakepassword1'), ('fakeuser2', 'fakepassword2'))
-
-
-def mock_authenticate(auth, remote, use_webservice):
-    try:
-        if use_webservice:
-            return users[auth]
-        else:
-            users[auth]
-            return {}
-    except Exception():
-        raise config.AuthFailedError()
+basic_user = dict(first_name="Jane", last_name="Doe", is_authenticated=True,
+        has_subscription=True)
+free_user = dict(first_name="John", last_name="Smith", is_authenticated=True,
+        has_subscription=False)
+anon_user = dict(is_authenticated=False)
+old_auth_user = {}
 
 
-class CheckedChangeAuthTestCaseBase(object):
+@patch('enstaller.config.change_auth')
+class CheckedChangeAuthTestCase(unittest.TestCase):
 
-    def mock_change_auth(self, username, password):
-        self.changed_auth = True
-
-    def commonSetUp(self):
-        """Replace authenticate and change_auth with mocks."""
-        self.old_authenticate = config.authenticate
-        self.old_change_auth = config.change_auth
-        config.change_auth = self.mock_change_auth
-        self.changed_auth = False
-
-    def commonTearDown(self):
-        """Restore authenticate and change_auth."""
-        config.authenticate = self.old_authenticate
-        config.change_auth = self.old_change_auth
-        self.changed_auth = False
-
-
-class CheckedChangeAuthWebserviceTestCase(unittest.TestCase, CheckedChangeAuthTestCaseBase):
-
-    def setUp(self):
-        self.commonSetUp()
-        config.authenticate = partial(mock_authenticate, use_webservice=True)
-
-    def test_basic_acct_good(self):
-        username, password = ('jdoe', 'password1')
-        usr = config.checked_change_auth(username, password)
-        self.assertTrue(self.changed_auth)
+    @patch('enstaller.config.authenticate', return_value=basic_user)
+    def test_basic_user(self, mock1, mock2):
+        usr = config.checked_change_auth('usr', 'password')
+        self.assertTrue(config.change_auth.called)
         self.assertTrue(usr.get('is_authenticated'))
         self.assertTrue(usr.get('has_subscription'))
 
-    def test_basic_acct_bad(self):
-        username, password = ('jdoe', 'wrong')
-        usr = config.checked_change_auth(username, password)
-        self.assertFalse(self.changed_auth)
-        self.assertEqual(usr, {})
-
-    def test_free_acct_good(self):
-        username, password = ('jsmith', 'password2')
-        usr = config.checked_change_auth(username, password)
-        self.assertTrue(self.changed_auth)
+    @patch('enstaller.config.authenticate', return_value=free_user)
+    def test_free_user(self, mock1, mock2):
+        usr = config.checked_change_auth('usr', 'password')
+        self.assertTrue(config.change_auth.called)
         self.assertTrue(usr.get('is_authenticated'))
         self.assertFalse(usr.get('has_subscription'))
 
-    def test_free_acct_bad(self):
-        username, password = ('jsmith', 'wrong')
-        usr = config.checked_change_auth(username, password)
-        self.assertFalse(self.changed_auth)
+    @patch('enstaller.config.authenticate',
+            side_effect=config.AuthFailedError())
+    def test_no_acct(self, mock1, mock2):
+        usr = config.checked_change_auth('usr', 'password')
+        self.assertFalse(config.change_auth.called)
+        self.assertFalse(usr.get('is_authenticated', False))
         self.assertEqual(usr, {})
 
-    def test_no_acct(self):
-        username, password = ('fakeuser', 'wrong')
-        usr = config.checked_change_auth(username, password)
-        self.assertFalse(self.changed_auth)
+    @patch('enstaller.config.authenticate', return_value=old_auth_user)
+    def test_remote_success(self, mock1, mock2):
+        usr = config.checked_change_auth('usr', 'password')
+        self.assertTrue(config.change_auth.called)
+        self.assertFalse(usr.get('is_authenticated', False))
         self.assertEqual(usr, {})
 
-    def test_empty_strings(self):
-        username, password = ('', '')
-        usr = config.checked_change_auth(username, password)
-        self.assertTrue(self.changed_auth)
+    @patch('enstaller.config.authenticate',
+            side_effect=config.AuthFailedError())
+    def test_nones(self, mock1, mock2):
+        usr = config.checked_change_auth(None, None)
+        self.assertTrue(config.change_auth.called)
+        self.assertFalse(usr.get('is_authenticated', False))
         self.assertEqual(usr, {})
 
-    def test_nones(self):
-        username, password = (None, None)
-        usr = config.checked_change_auth(username, password)
-        self.assertTrue(self.changed_auth)
+    @patch('enstaller.config.authenticate',
+            side_effect=config.AuthFailedError())
+    def test_empty_strings(self, mock1, mock2):
+        usr = config.checked_change_auth('', '')
+        self.assertTrue(config.change_auth.called)
+        self.assertFalse(usr.get('is_authenticated', False))
         self.assertEqual(usr, {})
 
-    def test_bad_users(self):
-        for auth in badusers:
-            usr = config.checked_change_auth(*auth)
-            self.assertFalse(self.changed_auth)
-            self.assertEqual(usr, {})
-
-    def tearDown(self):
-        self.commonTearDown()
-
-
-class CheckedChangeAuthNoWebserviceTestCase(unittest.TestCase, CheckedChangeAuthTestCaseBase):
-
-    def setUp(self):
-        """Replace authenticate and change_auth with mocks."""
-        self.commonSetUp()
-        config.authenticate = partial(mock_authenticate, use_webservice=False)
-
-    def test_good_users(self):
-        for auth in users.keys() + [('', ''), (None, None)]:
-            usr = config.checked_change_auth(*auth)
-            self.assertTrue(self.changed_auth)
-            self.assertEqual(usr, {})
-
-    def test_bad_users(self):
-        for auth in badusers:
-            usr = config.checked_change_auth(*auth)
-            self.assertFalse(self.changed_auth)
-            self.assertEqual(usr, {})
-
-    def tearDown(self):
-        self.commonTearDown()
 
 
 class SearchTestCase(unittest.TestCase):
