b1b4e152e5973f93fb34b8713a109ca99edd3117
David Cournapeau <cournape@gmail.com>
2013-11-26 16:01:36 +0000

    ENH: allow special value 'all' in list_dependencies platform argument.

--- scripts/list_dependencies.py
+++ scripts/list_dependencies.py
@@ -16,24 +16,9 @@ def get_default_remote(prefixes, plat):
     print "Using API URL {}".format(url)
     return RemoteHTTPIndexedStore(url, local_dir)
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-
-    userpass = config.get_auth()
-    plat = enstaller.plat.custom_plat
-
-    p = argparse.ArgumentParser()
-    p.add_argument("requirement",
-            help="Requirement string (e.g. 'mayavi')")
-    p.add_argument("--platform",
-            help="Platform to consider (default: %(default)s)",
-            default=plat)
-
-    namespace = p.parse_args(argv)
-
-    remote = get_default_remote([sys.prefix], namespace.platform)
-    req = Req(namespace.requirement)
+def query_platform(userpass, requirement, platform):
+    remote = get_default_remote([sys.prefix], platform)
+    req = Req(requirement)
 
     print "Connecting to remote repositories..."
     remote.connect(userpass)
@@ -50,6 +35,29 @@ def main(argv=None):
     root = resolver.get_egg(req)
     print("Resolving dependencies for {}: latest egg is {}".format(req, root))
     print_level(root)
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    userpass = config.get_auth()
+    plat = enstaller.plat.custom_plat
+
+    p = argparse.ArgumentParser()
+    p.add_argument("requirement",
+            help="Requirement string (e.g. 'mayavi')")
+    p.add_argument("--platform",
+            help="Platform to consider (default: %(default)s). 'all' works as well",
+            default=plat)
+
+    namespace = p.parse_args(argv)
+
+    if namespace.platform == "all":
+        platforms = ["rh5-32", "rh5-64", "osx-32", "osx-64", "win-32", "win-64"]
+        for platform in platforms:
+            query_platform(userpass, namespace.requirement, platform)
+    else:
+        query_platform(userpass, namespace.requirement, namespace.platform)
 
 if __name__ == "__main__":
     main()
