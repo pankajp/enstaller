639af2e2db64715507e86e3ca782595480862166
David Cournapeau <cournape@gmail.com>
2013-12-12 09:40:09 +0100

    TST: add simple tests for LocalIndexedStore.

--- enstaller/store/tests/test_store.py
+++ enstaller/store/tests/test_store.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import tempfile
@@ -10,7 +11,7 @@ import mock
 from okonomiyaki.repositories.enpkg import EnpkgS3IndexEntry
 
 from egginst.testing_utils import network
-from enstaller.store.indexed import RemoteHTTPIndexedStore
+from enstaller.store.indexed import LocalIndexedStore, RemoteHTTPIndexedStore
 
 from .common import DUMMY_EGG
 
@@ -46,3 +47,47 @@ class TestRemoteHTTPStore(unittest.TestCase):
         result = list(store.query(name="numpy"))
 
         self.assertTrue(len(result) > 1)
+
+class TestLocalIndexedStore(unittest.TestCase):
+    def setUp(self):
+        self.d = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.d)
+
+    def test_simple_query(self):
+        dummy_entry = EnpkgS3IndexEntry.from_egg(DUMMY_EGG, "free", True)
+        dummy_index = {
+            dummy_entry.egg_basename: dummy_entry.to_dict()
+        }
+
+        with open(op.join(self.d, "index.json"), "wt") as fp:
+            json.dump(dummy_index, fp)
+
+        store = LocalIndexedStore(self.d)
+        store.connect()
+
+        params = {"type": "egg"}
+        result = list(store.query(**params))
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][0], "dummy")
+
+        result = list(store.query_keys(**params))
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0], "dummy")
+
+        params = {"type": "fubar"}
+        result = list(store.query(**params))
+        self.assertEqual(len(result), 0)
+
+        result = list(store.query_keys(**params))
+        self.assertEqual(len(result), 0)
+
+    def test_get_data_missing_key(self):
+        with open(op.join(self.d, "index.json"), "wt") as fp:
+            json.dump({}, fp)
+
+        store = LocalIndexedStore(self.d)
+        store.connect()
+
+        self.assertRaises(KeyError, lambda: store.get_data("dummy_key"))
