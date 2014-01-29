e22a056d72ef196533e089720e7467bfb40141fa
Didrik Pinte <dpinte@enthought.com>
2014-01-29 21:57:29 +0100

    FIX run() should use the same python as the install/etc. (cherry picked from commit 3cd981e91fca49aa13bae30d8c3e4470e20a0388)

--- egginst/main.py
+++ egginst/main.py
@@ -401,7 +401,7 @@ class EggInst(object):
         if not isfile(path):
             return
         from subprocess import call
-        call([sys.executable, '-E', path, '--prefix', self.prefix],
+        call([scripts.executable, '-E', path, '--prefix', self.prefix],
              cwd=dirname(path))
 
 
