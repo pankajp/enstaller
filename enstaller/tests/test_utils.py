6a0cec8e0494ab3f4c0d6aa535838932a290742f
David Cournapeau <cournape@gmail.com>
2014-03-05 18:32:56 +0000

    BUG: label windows-specific failure as skipped, not expected failure.

--- enstaller/tests/test_utils.py
+++ enstaller/tests/test_utils.py
@@ -73,7 +73,7 @@ class TestUtils(unittest.TestCase):
         for url, r_url in r_data:
             self.assertEqual(cleanup_url(url), r_url)
 
-    @unittest.expectedFailure
+    @unittest.skipIf(sys.platform=="win32")
     def test_cleanup_url_dir(self):
         r_url = "file://{0}/".format(os.path.abspath(os.path.expanduser("~")))
 
